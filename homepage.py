737ab4654e1fdc0f6ecb311e73dbc17cb5ba5626
nineleaps <jaykgurjar@mail.com>
2022-10-11 14:28:10 +0530

    first official commit

--- homepage.py
+++ homepage.py
@@ -3,6 +3,3 @@ def Homepage():
     print("1.) Hey! leaper please LOGIN")
     print("2.) Hey! do you know we create account instantly. Please REGISTER to get welcome bonus upto Rs 1000*")
     print("3.) Exit page")
-
-
-Homepage()
