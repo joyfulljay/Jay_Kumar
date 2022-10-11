d0fc5427d6dd02d8689080df119f64e576d7c909
nineleaps <jaykgurjar@mail.com>
2022-10-11 20:28:33 +0530

    DBCREATION

--- databaseCreation.py
+++ databaseCreation.py
@@ -1,7 +1,8 @@
 from mysqlconnector import *
 
-d = mySQLcon()
+d = mySQLcon("BANKING")
 feedback = "create table feedback (session_id int AUTO_INCREMENT PRIMARY KEY, Login_timestamp datetime NOT NULL, Logout_timestamp datetime NOT NULL, Rating int NOT NULL, Additional_Comments varchar(100) NOT NULL)"
 feedback_row1_manual_input = """insert into feedback (session_id, Login_timestamp, Logout_timestamp, Rating, Additional_Comments) values(100001,now(),now(),4,"Good night")"""
-tansactions = "create table global_transactions (transaction_id varchar(20) PRIMARY KEY,transaction_time_stamp datetime not null default now(),Transaction_type enum(Debit,Credit)"
+global_transactions = """create table global_transactions (transaction_id varchar(20) PRIMARY KEY,transaction_time_stamp datetime not null default now(),Transaction_type enum("Debit","Credit"),Transaction_status enum("Succes","Failed"),Ammount bigint not null)"""
+Registeration = """create table Registeration (Account_no BIGINT PRIMARY KEY AUTO_INCREMENT, First_name varchar(20) NOT NULL,Last_name varchar(20) NOT NULL)"""
 d.run_query()
