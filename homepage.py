cc4ec746e674a61ce919e65eb716d4959e2af7d3
nineleaps <jaykgurjar@mail.com>
2022-10-11 14:27:06 +0530

    fourth commit

---
+++ homepage.py
@@ -0,0 +1,8 @@
+def Homepage():
+    print("{:-^100}".format("Welcome to Leap bank"))
+    print("1.) Hey! leaper please LOGIN")
+    print("2.) Hey! do you know we create account instantly. Please REGISTER to get welcome bonus upto Rs 1000*")
+    print("3.) Exit page")
+
+
+Homepage()
