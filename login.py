85233f44da80f23c0f792f18927eddbd1302b8a1
nineleaps <jaykgurjar@mail.com>
2022-10-12 19:20:08 +0530

    Login 1st comit

---
+++ login.py
@@ -0,0 +1,7 @@
+from homepage import *
+
+
+def login():
+    print("Bahut Jald hi Login sevayein chalu ho jayengi meanwhile register to kar hi dijiye")
+    input("Waps jane ke liye enter dabao: ")
+    Homepage()
