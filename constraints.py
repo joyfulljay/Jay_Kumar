5c1d4f72e122b474841f4954b37661607cf98fa3
nineleaps <jaykgurjar@mail.com>
2022-10-12 13:34:02 +0530

    upto email check

--- constraints.py
+++ constraints.py
@@ -1,10 +1,29 @@
 class constraints:
-    def integer(self, user_response):
-        if isinstance(user_response, int):
+
+    def spaces(self, user_response):
+        for i in user_response:
+            if ord(i) == 32:
+                print("oops! please avoid using spaces")
+                return False
+        return True
+
+    def length_constraint(self, user_response, leng):
+        if len(user_response) == leng:
             return True
         else:
-            print("oops, looks like you entered non integral value")
+            print(f"oops! input must be of {leng} length")
             return False
+
+    def integer(self, user_response):
+        """
+
+        :type user_response: Any
+        """
+        for i in user_response:
+            if (ord(i) < 48) and (ord(i) > 57):
+                print("oops! please respond with a integral values")
+                return False
+        return True
 
     def range(self, user_response, greater_than, lesser_than):
         if (user_response > greater_than) and (user_response < lesser_than):
@@ -16,7 +35,7 @@ class constraints:
         print("check1")
         print(ord('s'))
         if len(user_response) < 9:
-            print('Please enter atleast 9 character length of password')
+            print('Please enter at least 9 character length of password')
             return False
         else:
             count_numeric = 0
@@ -37,20 +56,67 @@ class constraints:
                 else:
                     count_special_char = count_special_char + 1
             if count_numeric == 0:
-                print('Please enter atleast single numeric value')
+                print('Please enter at least single numeric value')
                 return False
             else:
                 if count_special_char == 0:
-                    print('Please use atleast a special character')
+                    print('Please use at least a special character')
                     return False
                 else:
                     if count_small == 0:
-                        print('Please use atleast a lower case alphabet')
+                        print('Please use at least a lower case alphabet')
                         return False
                     else:
                         if count_capital == 0:
-                            print('Please use atleast a  upper case alphabet')
+                            print('Please use at least a  upper case alphabet')
                             return False
 
                         else:
                             return True
+
+    def Aadhar_card(self, user_response):
+        if self.spaces(user_response) and self.integer(user_response) and self.length(user_response, 12):
+            return True
+        else:
+            print("Invalid Aadhar")
+            return False
+
+    def Mobile_no(self, user_response):
+        if self.spaces(user_response) and self.integer(user_response) and self.length(user_response, 10):
+            return True
+        else:
+            print("Invalid Aadhar")
+            return False
+
+    def email_id(self, user_response):
+        if not self.spaces(user_response):
+            return False
+
+        poschar = user_response.find('@')  # position of '@'
+
+        if poschar == 0:
+            print("Invalid Email Address #1")
+            return False
+
+        poschar2 = user_response.find('.', poschar)  # position of '.'
+
+        Global_domains = [".org", ".com", ".in", ".net"]  # List of top level domains used in email address
+
+        if user_response.count('@') != 1:  # @ count of '@' should be 1
+            print("Invalid Email Address #2")
+            return False
+        else:
+            if user_response[poschar + 1] == '.':  #Check whether '.' is not just ahead of '@' or just checking if email domain is exist or not
+                print("Invalid Email Address #3")
+                return False
+            else:
+                if user_response[poschar::].count('.') != 1:  #Count of '.' after '@' should be 1
+                    print("Invalid Email Address #4")
+                    return False
+                else:
+                    Global_domain = user_response[user_response.find('.', poschar)::]  #Extracting global domain from user input email
+                    if Global_domain in Global_domains:  # Checking whether top level domain exists or not
+                        return True
+                    else:
+                        print("Invalid Email Address #5")
+                        return False
