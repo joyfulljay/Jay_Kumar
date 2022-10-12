b0d521eaace2a1061381d9cda4988211b892fdf6
nineleaps <jaykgurjar@mail.com>
2022-10-12 19:29:09 +0530

    todays final commit

--- Register.py
+++ Register.py
@@ -1,5 +1,6 @@
 from constraints import *
 from Additional_Query_functions import *
+from login import *
 
 
 class Registeration:
@@ -11,12 +12,15 @@ class Registeration:
         t = 0
         response = input("Enter Valid Mobile no. : +91 ")
 
-        elif self.constraint_obj.Mobile_no(response) :
+        if self.constraint_obj.Mobile_no(response):
 
             if self.query_obj.CheckInFunction("Mobile_no"):
                 print("Aap is no. se pehele se hi registered ho login karo ho jayega....")
-                return False
-
+                user_input = input("press ENTER to go on login page to retry with another mobile no. press any: ")
+                if bool(user_input):
+                    self.Mobile_input()
+                else:
+                    login()
             elif t > 3:
                 print("{: ^100}".format("\U0001F60C \U0001F60C \U0001F60C "))
                 print("{: ^100}".format("Mujhe pata tha bhai tu karlega"))
@@ -32,4 +36,5 @@ class Registeration:
             elif t == 3:
                 print("jab ho jaye tab batadena, mein sone jaa raha")
             else:
+                print("try again")
                 print("{: ^100}".format(" \U0001F634 \U0001F634 \U0001F634 \U0001F62A "))
