0949a68f0aeee64faa06c0850c96db44762755bb
nineleaps <jaykgurjar@mail.com>
2022-10-12 18:07:57 +0530

    Additional Querying class file 1st commit

--- Additional_Query_functions.py
+++ Additional_Query_functions.py
@@ -1,1 +1,18 @@
-class helper_query()
+from Data_Queries import *
+
+
+class helper_query:
+    def __init__(self, database, table_name):
+        self.database = database
+        self.table_name = table_name
+        self.obj = Data_queries(table_name, database)
+
+    def CheckInFunction(self, column_name, value):
+        values = self.obj.column(column_name)
+        if value in values:
+            return True
+        else:
+            return False
+
+    def map_value(self,to_map_column, to_map_on_value, map_with_column, value_to_be_checked):
+        print("will complete later")
